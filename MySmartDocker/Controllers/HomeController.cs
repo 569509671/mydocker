41d8ce47c348c953319b49488bae2d2721ed2c6b
569509671 <569509671@qq.com>
2021-01-16 16:23:33 +0800

    1

--- MySmartDocker/Controllers/HomeController.cs
+++ MySmartDocker/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace MySmartDocker.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.HelloKitty = "Docker";
+            ViewBag.Docker = "Docker";
             ViewBag.Times = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             return View();
         }
