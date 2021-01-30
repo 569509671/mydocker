1628c2c9aa30d793953d35e768804f3d0f5c465b
569509671 <569509671@qq.com>
2021-01-30 16:38:30 +0800

    2

--- MySmartDocker/Controllers/HomeController.cs
+++ MySmartDocker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Redis;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MySmartDocker.Models;
@@ -12,25 +13,29 @@ namespace MySmartDocker.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(/*ILogger<HomeController> logger,*/ IHttpContextAccessor httpContextAccessor)
         {
-            _logger = logger;
+            //_logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
         {
             ViewBag.Docker = "Hello World";
             var time = "Latest View Home Index Time Is :" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-            RedisHelper.StringSet("time", time);
+            var rdsKey = "Time:" + _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            RedisHelper.StringSet(rdsKey, time);
             return View();
         }
 
         public IActionResult SmartRedis()
         {
             ViewBag.Docker = "Docker World--Redis集群";
-            ViewBag.Times = RedisHelper.StringGet("time");
+            var rdsKey = "Time:" + _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            ViewBag.Times = RedisHelper.StringGet(rdsKey);
             return View();
         }
 
@@ -44,5 +49,6 @@ namespace MySmartDocker.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
